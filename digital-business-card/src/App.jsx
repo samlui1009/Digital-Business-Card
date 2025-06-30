e11375d9d68c6520c2a56ea428ec418e9ab4a75a
samlui1009 <sam_lui@hotmail.ca>
2025-06-29 17:35:45 -0700

    Adjusted more styling preferences (Larger font sizes - light/dark mode to be implemented later)

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card.jsx'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Card from './components/Card.jsx';
+// import Toggle from './components/Toggle.jsx';
 
 function App() {
+    const [isDark, setIsDark] = useState(true);
+    // Starting in dark mode, because isDark == true
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+    }, [isDark]);
+    // If isDark == true, set the theme to "dark"
+    // Otherwise, set the theme to "light"
 
   return (
     <>
-      <div className="homepage">
+      <div className="homepage" data-theme={isDark ? "dark" : "light"}>
+        {/* <Toggle 
+        isChecked={isDark}
+        handleChange={() => setIsDark(prev => !prev)}></Toggle>
+        Prev is the previous state of isDark, just a parameter name */}
         <Card></Card>
       </div>
     </>
---
+++ digital-business-card/src/components/Toggle.jsx
@@ -0,0 +1,24 @@
+// import './Toggle.css';
+
+// function Toggle({ handleChange, isChecked}) {
+//     return (
+//         <>
+//             <div className="toggle-container">
+//                 <input 
+//                     type="checkbox"
+//                     id="check"
+//                     className="toggle"
+//                     onChange={handleChange}
+//                     checked={isChecked}
+//                 />
+//                 <label htmlFor="check" className="toggle-label">
+//                     <div className="slider">
+//                         <span className="icon">{isChecked ? "🌙" : "🔆"}</span>
+//                     </div>
+//                 </label>
+//            </div>
+//         </>
+//     )
+// }
+
+// export default Toggle
